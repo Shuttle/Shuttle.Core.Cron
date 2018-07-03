b86f052f0636efbfdd040337b0b12e18b53688fa
eben-roux <me@ebenroux.co.za>
2018-07-03 19:25:00 +0200

    - v10.0.4

--- Shuttle.Core.Cron/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Cron/Properties/AssemblyInfo.cs
@@ -25,14 +25,18 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Core 2.0")]
 #endif
 
+#if NETCOREAPP2_1
+[assembly: AssemblyTitle(".NET Core 2.1")]
+#endif
+
 #if NETSTANDARD2_0
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.3.0")]
+[assembly: AssemblyVersion("10.0.4.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
 [assembly: AssemblyProduct("Shuttle.Core.Cron")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.3")]
+[assembly: AssemblyInformationalVersion("10.0.4")]
 [assembly: ComVisible(false)]
