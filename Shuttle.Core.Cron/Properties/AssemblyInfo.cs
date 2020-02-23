8a8faaf6fb0c1983a20b6dcb601f2ab1272127b2
Eben <me@ebenroux.co.za>
2020-02-23 17:26:00 +0200

    Update AssemblyInfo.cs

--- Shuttle.Core.Cron/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Cron/Properties/AssemblyInfo.cs
@@ -1,28 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if NET46
-[assembly: AssemblyTitle(".NET Framework 4.6")]
-#endif
-
 #if NET461
 [assembly: AssemblyTitle(".NET Framework 4.6.1")]
-#endif
-
-#if NET462
-[assembly: AssemblyTitle(".NET Framework 4.6.2")]
-#endif
-
-#if NET47
-[assembly: AssemblyTitle(".NET Framework 4.7")]
-#endif
-
-#if NET471
-[assembly: AssemblyTitle(".NET Framework 4.7.1")]
-#endif
-
-#if NETCOREAPP2_0
-[assembly: AssemblyTitle(".NET Core 2.0")]
 #endif
 
 #if NETCOREAPP2_1
