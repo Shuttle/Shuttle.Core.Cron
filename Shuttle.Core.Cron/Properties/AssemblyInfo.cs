6d83adc78f352b3fbffc0dc841c01e111f9b53c9
Eben <me@ebenroux.co.za>
2024-10-15 19:12:46 +0200

    v20 package

--- Shuttle.Core.Cron/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Cron/Properties/AssemblyInfo.cs
@@ -1,21 +1,11 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-#if NETFRAMEWORK
-[assembly: AssemblyTitle(".NET Framework")]
-#endif
 
-#if NETCOREAPP
-[assembly: AssemblyTitle(".NET Core")]
-#endif
-
-#if NETSTANDARD
-[assembly: AssemblyTitle(".NET Standard")]
-#endif
-
-[assembly: AssemblyVersion("10.1.3.0")]
+[assembly: AssemblyTitle(".NET Unified Platform")]
+[assembly: AssemblyVersion("20.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2024, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Core.Cron")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.1.3")]
+[assembly: AssemblyInformationalVersion("20.0.0")]
 [assembly: ComVisible(false)]
