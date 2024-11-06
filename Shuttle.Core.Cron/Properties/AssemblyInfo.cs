f63cc5c4afc219987ca6444e3c2c7ed9217c87e0
Eben <me@ebenroux.co.za>
2024-11-06 17:17:56 +0200

    Update AssemblyInfo.cs

--- Shuttle.Core.Cron/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Cron/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 #endif
 
 [assembly: AssemblyVersion("10.1.4.0")]
-[assembly: AssemblyCopyright("Copyright (c) #{Year}#, Eben Roux")]
+[assembly: AssemblyCopyright("Copyright (c) 2024, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Core.Cron")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("#{SemanticVersion}#")]
+[assembly: AssemblyInformationalVersion("10.1.4")]
 [assembly: ComVisible(false)]
